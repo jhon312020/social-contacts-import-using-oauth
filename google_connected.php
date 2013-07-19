2bc7e256e2516b52d6ea35bd949360148fc048c5
janakiraman <janakiraman@proisc.com>
2013-07-19 21:17:12 +0530

    Missed to include the config.php for google connect.  So added it.

--- google_connected.php
+++ google_connected.php
@@ -1,5 +1,6 @@
 <?php
 	session_start();
+	require_once('config.php');
 	require_once ('social_library/google/Google_Client.php');
 	$client = new Google_Client();
 	$client->setApplicationName('Google Contacts PHP Sample');
