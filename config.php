710d64bb6b59c5c33dc73a4b5e01bcd4f6265c5d
janakiraman <janakiraman@proisc.com>
2013-07-19 21:00:53 +0530

    Pushed the configuration file.

---
+++ config.php
@@ -0,0 +1,23 @@
+<?php
+	$yahoo_app_id = 'REPLACE WITH YAHOO APPLICATION ID';
+	$yahoo_consumer_key = 'REPLACE WITH YAHOO CONSUMER KEY';
+	$yahoo_consumer_secret= 'REPLACE WITH YAHOO CONSUMER SECRET';
+	$yahoo_connected_path = 'yahoo_connected.php';
+	$yahoo_redirect_url = 'REPLACE WITH REDIRECT URL';
+	
+	//example $yahoo_redirect_url = 'http://localhost/social_contacts/yahoo_connected.php';
+
+	$google_client_id = 'REPLACE WITH GOOGLE CLIENT ID';
+	$google_client_secret= 'REPLACE WITH CLIENT SECRET';
+	$google_connected_path = 'google_connected.php';
+	$google_redirect_url = 'REPLACE WITH REDIRECT URL';
+	//EXAMPLE $google_redirect_url = 'http://localhost/social_contacts/google_connected.php';
+
+	$msn_client_id = 'REPLACE WITH MSN CLIENT ID';
+	$msn_client_secret= 'REPLACE WITH MSN CLIENT SECRET';
+	$msn_connected_path = 'msn_connected.php';
+	$msn_redirect_url = 'REPLACE WITH REDIRECT URL';
+	
+	/* MSN WILL NOT WORK WITH localhost for that just refer this article	http://www.tonyamoyal.com/2009/08/17/how-to-quickly-set-up-a-test-for-twitter-oauth-authentication-from-your-local-machine/ */
+	//EXAMPLE $msn_redirect_url = 'http://hint.com/www/social_contacts/msn_connected.php';
+?>
